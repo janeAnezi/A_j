3468edb022728d853f2482b4b218e732005f8305
Anezijane <ahnnieanezi@gmail.com>
2024-10-24 22:00:22 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>contact</section>
+    </>
+  )
+}
+
+export default App
