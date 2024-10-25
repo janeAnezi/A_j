9401369897e1820ababfb862c8b42b747a353f0b
Anezijane <ahnnieanezi@gmail.com>
2024-10-25 08:17:45 +0100

    import social icons

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,10 +7,10 @@ export default function NavBar() {
         <div className="wrapper">
             <span>Anezi_Jane</span>
             <div className="social">
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="../assets/images/facebook.jpeg" alt="a facebook icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="../assets/images/twitter.png" alt="a twitter icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="../assets/images/linkdin.png" alt="a linkedin icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="../assets/images/whatsapp.png" alt="a whatsapp icon" /></a>
+                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/facebook.jpeg" alt="a facebook icon" /></a>
+                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/twitter.png" alt="a twitter icon" /></a>
+                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/linkdin.png" alt="a linkedin icon" /></a>
+                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/whatsapp.png" alt="a whatsapp icon" /></a>
             </div>
         </div>
       
