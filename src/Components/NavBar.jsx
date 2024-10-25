5e746847074a073764fdd434589921b152682ed6
Anezijane <ahnnieanezi@gmail.com>
2024-10-25 09:14:34 +0100

    add social reference

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,10 +7,10 @@ export default function NavBar() {
         <div className="wrapper">
             <span>Anezi_Jane</span>
             <div className="social">
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/facebook.jpeg" alt="a facebook icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/twitter.png" alt="a twitter icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/linkdin.png" alt="a linkedin icon" /></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><img src="/images/whatsapp.png" alt="a whatsapp icon" /></a>
+                <a href="https://github.com/janeAnezi?tab=repositories" target="_blank" rel="noopener noreferrer"><img src="/images/github.jpeg" alt="a facebook icon" /></a>
+                <a href="https://x.com/AhnnieA" target="_blank" rel="noopener noreferrer"><img src="/images/twitter.png" alt="a twitter icon" /></a>
+                <a href="https://www.linkedin.com/in/jane-anezi-a9771a238/"  target="_blank" rel="noopener noreferrer"><img src="/images/linkdin.png" alt="a linkedin icon" /></a>
+                <a href="tel:+2347053882420"><img src="/images/whatsapp.png" alt="a whatsapp icon" /></a>
             </div>
         </div>
       
