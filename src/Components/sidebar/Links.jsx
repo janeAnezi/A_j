75f78895bf760200db0eb5fb7ba0170100ef4e26
Anezijane <ahnnieanezi@gmail.com>
2024-10-26 10:59:34 +0100

    add animation to side bar links

--- src/Components/sidebar/Links.jsx
+++ src/Components/sidebar/Links.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import {motion} from 'framer-motion'
 
 export default function Links() {
+  const variants = {
+    open: {
+      transition: {
+        staggerChildren: 0.1,
+      }
+    },
+    closed: {
+      transition: {
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      }
+    }
+  }
+  const itemVariants = {
+    open: {
+     y: 0,
+     opacity: 1
+    },
+    closed: {
+      y: 50,
+      opacity: 0
+    }
+  }
 
-  let itemsLink = [
-    "HomePage",
-    "Services",
-    "Portfolio",
-    "Contact",
-  ]
+  let itemsLink = ["HomePage","Services","Portfolio","Contact",]
 
   return (
-    <div className='links'>
+    <motion.div className='links' variants={variants}>
       {itemsLink.map((item)=> (
-        <a href={`#${item}`} key={item}>{item}</a>
+        <motion.a href={`#${item}`} key={item} variants={itemVariants}>{item}</motion.a>
       ))}  
-    </div>
+    </motion.div>
   )
 }
--- src/Components/sidebar/ToggleButton.jsx
+++ src/Components/sidebar/ToggleButton.jsx
@@ -1,11 +1,36 @@
+import { motion } from "framer-motion";
 
 export default function ToggleButton({ setOpen }) {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
-        <path d="M 2 2.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <path d="M 2 11.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <path d="M 2 20.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <motion.path 
+          strokeWidth="3" 
+          stroke="black" 
+          strokeLinecap="round" 
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }} 
+        />
+        <motion.path 
+          strokeWidth="3" 
+          stroke="black" 
+          strokeLinecap="round" 
+          variants={{
+            closed: { d: "M 2 11.5 L 20 11.5", opacity: 1 }, 
+            open: { opacity: 0 },
+          }} 
+        />
+        <motion.path 
+          strokeWidth="3" 
+          stroke="black" 
+          strokeLinecap="round" 
+          variants={{
+            closed: { d: "M 2 20.5 L 20 20.5" },
+            open: { d: "M 3 2.5 L 17 16.5" },
+          }} 
+        />
       </svg>
     </button>
   );
