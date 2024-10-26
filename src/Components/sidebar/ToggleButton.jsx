bde5ae28f83d91dac4c170843ea887597fe177ae
Anezijane <ahnnieanezi@gmail.com>
2024-10-26 10:07:43 +0100

    add button svg

--- src/Components/sidebar/ToggleButton.jsx
+++ src/Components/sidebar/ToggleButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
-export default function ToggleButton({setOpen}) {
+export default function ToggleButton({ setOpen }) {
   return (
-    <button onClick={()=> setOpen((prev) => !prev)}>
-      Button
+    <button onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <path d="M 2 2.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <path d="M 2 11.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <path d="M 2 20.5 H 20" strokeWidth="3" stroke="black" strokeLinecap="round" />
+      </svg>
     </button>
-  )
+  );
 }
